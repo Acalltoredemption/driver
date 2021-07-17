23de3c3fa915e23a2c8784eb05c7cd0e3f394531
Acalltoredemption <acalltoredemption@gmail.com>
2021-07-16 17:14:47 -0700

    first commit

---
+++ src/components/backdrop/backdrop.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './backdrop.css';
+
+
+const Backdrop = () => {
+    return ( 
+    <div>
+        <div className="backdrop-grass"> 
+        <div className="road"/>
+        </div>
+        <div className="backdrop-sky"></div>
+    </div>
+    );
+}
+
+export default Backdrop;
